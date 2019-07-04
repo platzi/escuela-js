5e8eeb9d8b5ba11504916b728a67239b2e11103c
Oscar Barajas <oscar@dragons.mx>
2019-07-04 01:06:16 -0500

    Player

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { setFavorite } from "../actions";
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png'
 
 const CarouselItem = (props) => {
-  const { cover, title, year, contentRating, duration, isList } = props;
+  const { cover, title, year, contentRating, duration, isList, slug, source } = props;
   const handleSetFavorite = () => {
     props.setFavorite({
-      cover, title, year, contentRating, duration,
-    })
+      cover, title, year, contentRating,
+      duration, isList, slug, source
+    });
   }
   return (
     <div className="carousel-item">
       <img className="carousel-item__img" src={cover} alt={title} />
       <div className="carousel-item__details">
         <div>
-          <img className="carousel-item__details--img" src={playIcon} alt="Play Icon" />
+          <Link to={{
+            pathname: `/player/${slug}`,
+            state: { source }
+          }}
+          >
+            <img className="carousel-item__details--img" src={playIcon} alt="Play Icon" />
+          </Link>
           {!isList ? (
             <img
               className="carousel-item__details--img"
@@ -35,6 +43,7 @@ const CarouselItem = (props) => {
     </div>
   );
 }
+
 CarouselItem.propTypes = {
   title: PropTypes.string,
   year: PropTypes.number,
