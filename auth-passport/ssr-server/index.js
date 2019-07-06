d7e878915d03d3db1ad4111d0451a78b13bb250c
Guillermo Rodas <garethderioth@gmail.com>
2019-07-06 01:31:46 -0500

    Update endpoints

--- auth-passport/ssr-server/index.js
+++ auth-passport/ssr-server/index.js
@@ -4,6 +4,7 @@ const boom = require("@hapi/boom");
 const cookieParser = require("cookie-parser");
 const debug = require("debug")("app:server");
 const axios = require("axios");
+const { get } = require("lodash");
 
 const { config } = require("./config");
 
@@ -64,16 +65,40 @@ app.get("/movies", async function(req, res, next) {
   const { token } = req.cookies;
 
   try {
-    const { data, status } = await axios({
+    const moviesRequest = axios({
       url: `${config.apiUrl}/api/movies`,
       headers: { Authorization: `Bearer ${token}` }
     });
 
-    if (!data || status !== 200) {
-      next(boom.badImplementation());
+    const userMoviesRequest = axios({
+      url: `${config.apiUrl}/api/user-movies?userId=${userId}`,
+      headers: { Authorization: `Bearer ${token}` }
+    });
+
+    const [moviesResponse, userMoviesResponse] = await Promise.all([
+      moviesRequest,
+      userMoviesRequest
+    ]);
+
+    const moviesStatus = get(moviesResponse, "status");
+    const userMoviesStatus = get(userMoviesResponse, "status");
+
+    if (moviesStatus !== 200 || userMoviesStatus !== 200) {
+      return next(boom.badImplementation());
     }
 
-    res.status(200).json(data);
+    const moviesData = get(moviesResponse, "data.data", []);
+    const userMoviesData = get(userMoviesResponse, "data.data", []);
+
+    const userMoviesDataMapped = userMoviesData.map(userMovie => ({
+      ...moviesData.find(movie => movie._id),
+      userMovieId: userMovie._id
+    }));
+
+    res.status(200).json({
+      myList: userMoviesDataMapped,
+      movies: moviesData
+    });
   } catch (error) {
     next(error);
   }
@@ -83,18 +108,35 @@ app.post("/user-movies", async function(req, res, next) {
   const { body: userMovie } = req;
   const { token } = req.cookies;
 
-  // TODO: Implement and call the api
+  const { data, status } = await axios({
+    url: `${config.apiUrl}/api/user-movies`,
+    headers: { Authorization: `Bearer ${token}` },
+    method: "post",
+    data: userMovie
+  });
 
-  res.status(200).json({});
+  if (status !== 201) {
+    return next(boom.badImplementation());
+  }
+
+  res.status(201).json(data);
 });
 
 app.delete("/user-movies/:userMovieId", async function(req, res, next) {
   const { userMovieId } = req.params;
   const { token } = req.cookies;
-  
-  // TODO: Implement and call the api
 
-  res.status(200).json({});
+  const { data, status } = await axios({
+    url: `${config.apiUrl}/api/user-movies/${userMovieId}`,
+    headers: { Authorization: `Bearer ${token}` },
+    method: "delete"
+  });
+
+  if (status !== 200) {
+    return next(boom.badImplementation());
+  }
+
+  res.status(200).json(data);
 });
 
 app.listen(config.port, function() {
